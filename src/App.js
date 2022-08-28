92f8684380f4f6655bcb6ab36e652d54fc8f561e
cowoakvalley <kdegelund@gmail.com>
2022-08-28 18:27:22 +0200

    Created search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
+      <div className="container">
+        <header className="App-header text-center">
+          <h1>DICTIONARY</h1>
+        </header>
+        <Dictionary />
+        <footer className="text-center">
+          <p>
+            open source coded by{" "}
+            <a href="https://github.com/cowoakvalley/dictionary-project">
+              cowoakvalley
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
